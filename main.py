8cf28477daefbe35a66c086b30086b75e7aba7d4
Shagwu <sharon.agwu@gmail.com>
2025-04-26 17:48:33 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# /// script
+# requires-python = "==3.9.21"
+# dependencies = [
+#     "request",
+#     "rich",
+# ]
+# ///
+
+# /// script
+# requires-python = "==3.9.21"
+# dependencies = [
+#     "requests",
+#     "rich",
+#     "beautifulsoup4",
+#     "lxml",
+#     "pandas",
+#     "openpyxl",
+#     "pytz",   
